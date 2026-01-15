ce6cb4590524f0be2ca018d6f4474968a7d00364
grcubes <gustavosamrod@gmail.om>
2026-01-15 03:23:29 -0800

    :feat: Create projects data and display dynamically

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+        name: "Berry-O-Ware",
+        img: "./assets/project-covers/ph.png",
+        desc: "A cool epic game",
+    },
+    {
+        name: "Berry-O-Ware",
+        img: "./assets/project-covers/ph.png",
+        desc: "A cool epic game",
+    },
+    {
+        name: "Berry-O-Ware",
+        img: "./assets/project-covers/ph.png",
+        desc: "A cool epic game",
+    },
+    {
+        name: "Berry-O-Ware",
+        img: "./assets/project-covers/ph.png",
+        desc: "A cool epic game",
+    },
+    {
+        name: "Berry-O-Ware",
+        img: "./assets/project-covers/ph.png",
+        desc: "A cool epic game",
+    },
+    {
+        name: "Berry-O-Ware",
+        img: "./assets/project-covers/ph.png",
+        desc: "A cool epic game",
+    },
+    {
+        name: "Berry-O-Ware",
+        img: "./assets/project-covers/ph.png",
+        desc: "A cool epic game",
+    },
+    {
+        name: "Berry-O-Ware",
+        img: "./assets/project-covers/ph.png",
+        desc: "A cool epic game",
+    },
+]
---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import { projects } from '../data/projects.js';
+
+function calculateMainTopMargin()
+{
+    const header = document.querySelector("header");
+    const navbar = document.querySelector("nav");
+    
+    const main = document.querySelector("main");
+    const top_margin = header.offsetHeight - navbar.offsetHeight;
+    
+    main.style.marginTop = `${top_margin}px`;
+}
+
+function displayProjects()
+{
+    const projectContainer = document.querySelector("#projects");
+    const template = document.querySelector("#project-template");
+
+    projects.forEach(project => {
+        const projectInstance = template.content.cloneNode(true);
+
+        projectInstance.querySelector(".project-title").textContent = project.name;
+        projectInstance.querySelector(".project-desc").textContent = project.desc;
+
+        const img = projectInstance.querySelector(".project-img");
+        img.src = project.img;
+
+        projectContainer.appendChild(projectInstance);
+    })
+}
+
+
+function setup()
+{
+    calculateMainTopMargin();
+    displayProjects();
+}
+
+setup();
